a39e5c217490cb58bc973c99c9aba78cf27c89cf
Julius Volz <julius@soundcloud.com>
2015-09-30 21:27:08 +0200

    Support escape sequences in strings and add raw strings.
    This adapts some functionality from the Go standard library for string
    literal lexing and unquoting/unescaping.
    
    The following string types are now supported:
    
    Double- or single-quoted strings:
    
      These support all escape sequences that Go supports in double-quoted
      string literals. The difference is that Prometheus also has
      single-quoted strings (instead of single-quoted runes in Go). Raw
      newlines are not allowed.
    
    Backtick-quoted raw strings:
    
      Strings quoted in backticks are treated as raw strings just like in Go
      and may contain raw newlines and other special characters directly.
    
    Fixes https://github.com/prometheus/prometheus/issues/1122
    Fixes https://github.com/prometheus/prometheus/issues/1121

--- util/strutil/quote.go
+++ util/strutil/quote.go
@@ -103,7 +103,7 @@ func unquoteChar(s string, quote byte) (value rune, multibyte bool, tail string,
 		return rune(s[0]), false, s[1:], nil
 	}
 
-	// hard case: c is backslash
+	// Hard case: c is backslash.
 	if len(s) <= 1 {
 		err = ErrSyntax
 		return
@@ -151,7 +151,7 @@ func unquoteChar(s string, quote byte) (value rune, multibyte bool, tail string,
 		}
 		s = s[n:]
 		if c == 'x' {
-			// single-byte string, possibly not UTF-8
+			// Single-byte string, possibly not UTF-8.
 			value = v
 			break
 		}
@@ -167,7 +167,7 @@ func unquoteChar(s string, quote byte) (value rune, multibyte bool, tail string,
 			err = ErrSyntax
 			return
 		}
-		for j := 0; j < 2; j++ { // one digit already; two more
+		for j := 0; j < 2; j++ { // One digit already; two more.
 			x := rune(s[j]) - '0'
 			if x < 0 || x > 7 {
 				err = ErrSyntax
